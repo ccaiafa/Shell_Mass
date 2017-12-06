0783f523471a5d4048b44e144e023f4841a897dd
ccaiafa <ccaiafa@gmail.com>
2017-12-05 20:29:10 -0500

    new analysis with upper bound

--- Scripts_paper/Analyze_4c_total.m
+++ Scripts_paper/Analyze_4c_total.m
@@ -3,7 +3,9 @@ restoredefaultpath
 cd('/Users/CesarMB13/Google Drive/My Journal papers/In preparation/Shell_mass/code/Shell_Mass/Scripts_paper')
 close all
 
-data_type = '4c_total';
+%data_type = '4c_total';
+data_type = '4c_total_dR_0.165';
+dR = 0.165;
 
 cd ..
 code_path = pwd;
@@ -16,7 +18,7 @@ dataRootPath = strcat(root,'/data');
 
 dataOutPath = InputPath;
 
-Analyze_masses_new(InputPath, data_type, dataRootPath, dataOutPath)
+Analyze_masses_new(InputPath, data_type, dataRootPath, dataOutPath, dR)
 
 rmpath(genpath(code_path))
 
