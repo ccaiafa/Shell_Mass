3f0298cdc96bbc009050207a38c48b3951977ba4
Cesar Caiafa <ccaiafa@gmail.com>
2017-12-12 12:15:41 -0500

    new results added

---
+++ Scripts_paper/Compute_3c_total_080.m
@@ -0,0 +1,32 @@
+% 
+clear
+clc
+restoredefaultpath
+
+clear
+clc
+
+cd ..
+code_path = pwd;
+addpath(genpath(code_path));
+
+cd ../..
+root = pwd;
+dataOutPath = strcat(root,'/code/Shell_Mass/Results');
+dataRootPath = strcat(root,'/data');
+
+OutputFile = 'Results_3c_total';
+
+dR = 0.80;
+
+alpha_range = 0.2:0.01:1.0;
+%alpha_range = [0.15,0.18];
+delta_range = 0:0.01:0.75;
+%delta_range = [0.15, 0.25];
+
+Visualization = 0; % Visualization ONLY works in Cubo 1 (long range 80 - 130)
+perc = 0.7; % 70% of channels
+
+run_all_masses(dataRootPath, @load_shells_3c_total, dataOutPath, OutputFile, alpha_range, delta_range, Visualization, perc, dR);
+
+rmpath(genpath(code_path));
